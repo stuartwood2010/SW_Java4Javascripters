665439b974de151b366e4203f713e07414c8f0bc
Stuart Wood <stuartwood2010@hotmail.com>
2022-04-13 21:43:55 -0600

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/J4JDB', {
+mongoose.connect(
+	process.env.MONGODB_URI || 'mongodb://localhost:27017/J4JDB', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
